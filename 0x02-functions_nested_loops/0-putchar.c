6c214da94ffbb802d8f67346bc0e4f87e4c2c065
Yanosan12 <yassinenajii90@gmail.com>
2023-03-22 17:37:45 -0700

    fgfgfgfgfgfg

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
+#include "main.h"
 /**
- * main - Entry point
- *
- * This function prints the string "_putchar" to standard output
- *
- * Return: Always 0 (Success)
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
+
 int main(void)
 {
 _putchar('_');
